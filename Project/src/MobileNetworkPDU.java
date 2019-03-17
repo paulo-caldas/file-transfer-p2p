25904fa743c47ce7874fc7272e781e42f3be8891
MDingas <a79089@alunos.uminho.pt>
2019-03-17 16:07:16 +0000

    :sparkles: skeleton of PDU structure

--- Project/src/MobileNetworkPDU.java
+++ Project/src/MobileNetworkPDU.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class MobileNetworkPDU {
+
+    private byte[] srcMAC;
+    private byte[] dstMAC;
+    private String contentID;
+    private String sessionID;
+    private MobileNetworkErrorType errorCode;
+    private MobileNetworkMessageType messageType;
+    private int TTL;
+    private int payloadLen;
+    private String[] routerPath;
+    private String payload; // JSON string, null if its of type ping or pong
 }
