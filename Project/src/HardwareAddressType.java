5e90bcfd739b42b956b9f470f195ba7099980624
MDingas <a79089@alunos.uminho.pt>
2019-03-17 16:48:36 +0000

    :sparkles: enum of MAC addrs

--- Project/src/HardwareAddressType.java
+++ Project/src/HardwareAddressType.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public enum HardwareAddressType {
+    BROADCAST("FF:FF:FF:FF:FF:FF"),
+    MULTICAST("FF:FF:FF:FF:FF:FF")
+    ;
+
+    private final String text;
+
+    /**
+     * @param text
+     */
+    HardwareAddressType(final String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
 }
