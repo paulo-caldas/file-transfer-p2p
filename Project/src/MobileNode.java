a0b21ecedf164ac9502ab29c3985a86dd42ce664
MDingas <a79089@alunos.uminho.pt>
2019-03-29 15:23:55 +0000

    :sparkles: now mapping content id to the nodes that provide it, addind new communication methods

--- Project/src/MobileNode.java
+++ Project/src/MobileNode.java
@@ -1,25 +1,39 @@
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.net.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+/*
+ *
+ *   | Content-ID |   Node-IDs
+ *   | 123        |   999,888
+ *   | 789        |   777
+ *
+ */
 
 public class MobileNode {
+    DatagramSocket serverSocket;
+    DatagramPacket receivePacket;
+    DatagramPacket sendPacket;
+    byte[] buffer;
     private String macAddr;
-    private List<String> peerMacAddrs;
+    private Map<String,List<String>> contentTable; // Maps content IDs to the nodes that provide them
 
     public MobileNode() {
         try {
             this.macAddr = macByteArrToString(NetworkInterface.getNetworkInterfaces().nextElement().getHardwareAddress());
-            System.out.println(this.macAddr);
+            this.serverSocket = new DatagramSocket(Integer.parseInt(AddressType.LISTENING_PORT.toString()));
+            buffer = new byte[1024];
+            this.contentTable = new HashMap<>();
         } catch (SocketException e) {
             e.printStackTrace();
         }
-        this.peerMacAddrs = new ArrayList<>();
     }
 
-    public MobileNode(String macAddr, List<String> peerList) {
+    public MobileNode(String macAddr, Map<String,List<String>> contentTable) {
         this.macAddr = macAddr;
-        this.peerMacAddrs = peerList;
+        this.contentTable = contentTable;
     }
 
     private String macByteArrToString(byte[] mac) {
@@ -32,16 +46,31 @@ public class MobileNode {
         return sb.toString();
     }
 
-    public void sendHelloMessage(String dstMAC) {
-
+    public void sendHelloMessage(String dstMac) {
+        try {
+            buffer = new String("hello from " + this.macAddr + " to " + dstMac).getBytes();
+            sendPacket = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(AddressType.NETWORK_BROADCAST.toString()), Integer.parseInt(AddressType.LISTENING_PORT.toString()));
+            serverSocket.send(sendPacket);
+        } catch (IOException e) {
+          }
     }
 
     public void sendPingMessage(String dstMac) {
-
+        try {
+            buffer = new String("ping from " + this.macAddr + " to " + dstMac).getBytes();
+            sendPacket = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(AddressType.NETWORK_BROADCAST.toString()), Integer.parseInt(AddressType.LISTENING_PORT.toString()));
+            serverSocket.send(sendPacket);
+        } catch (IOException e) {
+        }
     }
 
     public void sendPongMessage(String dstMac) {
-
+        try {
+            buffer = new String("pong from " + this.macAddr + " to " + dstMac).getBytes();
+            sendPacket = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(AddressType.NETWORK_BROADCAST.toString()), Integer.parseInt(AddressType.LISTENING_PORT.toString()));
+            serverSocket.send(sendPacket);
+        } catch (IOException e) {
+        }
     }
 
     // Query if my peers are alive or not by sending pings and awaiting for pongs
@@ -53,11 +82,22 @@ public class MobileNode {
 
     }
 
+    public void listenForPeers() {
+        try {
+            while(true) {
+                serverSocket.receive(receivePacket);
+                String data = new String(receivePacket.getData());
+
+                System.out.println(data);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void run() {
-
-        sendHelloMessage(HardwareAddressType.MULTICAST.toString());
-
-        // Be idle for incoming messages
-
+        sendHelloMessage(AddressType.LINK_MULTICAST.toString());
+        new MobileNodeKeepaliveDaemon(this).run();
+        listenForPeers();
     }
 }
