41068442ebbbadb78631270efcb0d208bcd115fa
MDingas <a79089@alunos.uminho.pt>
2019-03-31 15:17:57 +0100

    :art:

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("- Starting main");
         MobileNode node = new MobileNode();
-        node.run();
+        try {
+            node.run();
+        } catch (InterruptedException e) { }
     }
 }
