839018d68aab213faf7059373183a47c0d5dacba
MDingas <a79089@alunos.uminho.pt>
2019-03-31 04:09:54 +0100

    :memo: notes

---
+++ Project/src/PDU/HelloMobileNetworkPdu.java
@@ -0,0 +1,4 @@
+package PDU;
+
+public class HelloMobileNetworkPdu {
+}
---
+++ Project/src/PDU/MobileNetworkPDU.java
@@ -0,0 +1,47 @@
+import Enum.*;
+
+import java.io.Serializable;
+
+public class MobileNetworkPDU implements Serializable {
+
+    private String srcMAC;
+    private String dstMAC;
+    private MobileNetworkMessageType messageType;
+    private MobileNetworkErrorType errorCode;
+    private int TTL;
+    private String sessionID;
+
+    public MobileNetworkPDU(String srcMAC, String dstMAC, MobileNetworkMessageType messageType, MobileNetworkErrorType errorCode, int TTL, String sessionID) {
+        this.srcMAC = srcMAC;
+        this.dstMAC = dstMAC;
+        this.messageType = messageType;
+        this.errorCode = errorCode;
+        this.TTL = TTL;
+        this.sessionID = sessionID;
+    }
+
+    public String getSrcMAC() {
+        return srcMAC;
+    }
+
+    public String getDstMAC() {
+        return dstMAC;
+    }
+
+    public MobileNetworkMessageType getMessageType() {
+        return messageType;
+    }
+
+    public MobileNetworkErrorType getErrorCode() {
+        return errorCode;
+    }
+
+    public int getTTL() {
+        return TTL;
+    }
+
+    public String getSessionID() {
+        return sessionID;
+    }
+
+}
