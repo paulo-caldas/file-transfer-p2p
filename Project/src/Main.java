58dd0705045e563e3c7314c31f91adf37e41fafb
MDingas <a79089@alunos.uminho.pt>
2019-04-08 16:23:58 +0100

    receives sharing path as argument

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,9 +1,23 @@
+
+import main.MobileNode;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java -jar AERProj.jar <directory of content to share>");
+            System.exit(0);
+        }
+        File sharingDirectory = new File(args[0]).getAbsoluteFile();
+
         System.out.println("- Starting main");
-        MobileNode node = new MobileNode();
+
         try {
-            node.run();
-        } catch (InterruptedException e) { }
+            new MobileNode(sharingDirectory).run();
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
