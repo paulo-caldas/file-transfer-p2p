1257a3a14431c43a892e019fbd02c3cb827ae207
MDingas <a79089@alunos.uminho.pt>
2019-04-13 00:37:12 +0100

    :sparkles: now shares new type of routing table

--- Project/src/main/java/Business/PDU/HelloMobileNetworkPDU.java
+++ Project/src/main/java/Business/PDU/HelloMobileNetworkPDU.java
@@ -1,18 +1,21 @@
 package Business.PDU;
 
-import Business.ContentReference;
 import Business.Enum.MobileNetworkMessageType;
 import Business.Enum.MobileNetworkErrorType;
+import Business.MobileRoutingTableEntry;
 
-import java.util.List;
 import java.util.Map;
 
 public class HelloMobileNetworkPDU extends MobileNetworkPDU {
+    private Map<String, MobileRoutingTableEntry> contentRoutingTable;
 
-    Map<String, List<ContentReference>> contentMap;
-
-    public HelloMobileNetworkPDU(String srcMAC, String dstMAC, MobileNetworkMessageType messageType, MobileNetworkErrorType errorCode, int TTL, String sessionID, Map<String, List<ContentReference>> contentMap) {
+    public HelloMobileNetworkPDU(String srcMAC, String dstMAC, MobileNetworkMessageType messageType, MobileNetworkErrorType errorCode, int TTL, String sessionID, Map<String, MobileRoutingTableEntry> contentRoutingTable) {
         super(srcMAC, dstMAC, messageType, errorCode, TTL, sessionID);
-        this.contentMap = contentMap;
+        this.contentRoutingTable = contentRoutingTable;
     }
+
+    public Map<String, MobileRoutingTableEntry> getContentRoutingTable() {
+        return contentRoutingTable;
+    }
+
 }
