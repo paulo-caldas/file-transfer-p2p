01e570c55a5efa60b07fa44e242992776fbbd7e0
MDingas <a79089@alunos.uminho.pt>
2019-04-17 00:51:39 +0100

    :art: using updated class name

--- Project/src/main/java/Business/PDU/HelloMobileNetworkPDU.java
+++ Project/src/main/java/Business/PDU/HelloMobileNetworkPDU.java
@@ -1,20 +1,18 @@
 package Business.PDU;
 
+import Business.ContentRoutingTable;
 import Business.Enum.MobileNetworkMessageType;
 import Business.Enum.MobileNetworkErrorType;
-import Business.MobileRoutingTableEntry;
-
-import java.util.Map;
 
 public class HelloMobileNetworkPDU extends MobileNetworkPDU {
-    private Map<String, MobileRoutingTableEntry> contentRoutingTable;
+    private ContentRoutingTable contentRoutingTable;
 
-    public HelloMobileNetworkPDU(String srcMAC, String dstMAC, MobileNetworkMessageType messageType, MobileNetworkErrorType errorCode, int TTL, String sessionID, Map<String, MobileRoutingTableEntry> contentRoutingTable) {
+    public HelloMobileNetworkPDU(String srcMAC, String dstMAC, MobileNetworkMessageType messageType, MobileNetworkErrorType errorCode, int TTL, String sessionID, ContentRoutingTable contentRoutingTable) {
         super(srcMAC, dstMAC, messageType, errorCode, TTL, sessionID);
         this.contentRoutingTable = contentRoutingTable;
     }
 
-    public Map<String, MobileRoutingTableEntry> getContentRoutingTable() {
+    public ContentRoutingTable getContentRoutingTable() {
         return contentRoutingTable;
     }
 
