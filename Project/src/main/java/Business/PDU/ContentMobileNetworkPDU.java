9d77f49df3e01519a0171e40dc8ca3698068678b
MDingas <a79089@alunos.uminho.pt>
2019-04-29 15:47:33 +0100

    content path is now array instead of list, more efficient

--- Project/src/main/java/Business/PDU/ContentMobileNetworkPDU.java
+++ Project/src/main/java/Business/PDU/ContentMobileNetworkPDU.java
@@ -1,15 +1,14 @@
 package Business.PDU;
 
 
-import java.util.List;
-
-
 public class ContentMobileNetworkPDU extends MobileNetworkPDU {
-    String contentID;
-    List<String> nodePath;
+    String[] nodePath;
 
-    public ContentMobileNetworkPDU(String srcMAC, String dstMAC, MobileNetworkMessageType messageType, MobileNetworkErrorType errorCode, int TTL, String sessionID, String contentID) {
+    public ContentMobileNetworkPDU(String srcMAC, String dstMAC, MobileNetworkMessageType messageType, MobileNetworkErrorType errorCode, int TTL, String sessionID, String[] nodePath) {
             super(srcMAC, dstMAC, messageType, errorCode, TTL, sessionID);
-
+            this.nodePath = nodePath;
     }
+
+    public String[] getNodePath() { return nodePath; }
+    public void setNodePath(String[] nodePath) { this.nodePath = nodePath; }
 }
