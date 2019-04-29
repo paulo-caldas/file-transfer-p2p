e94d6c39ce5d69a1f6aab31544dbbe514ccc8e22
MDingas <a79089@alunos.uminho.pt>
2019-04-29 15:49:53 +0100

    :art: final classes

--- Project/src/main/java/Business/MobileNetworkNode/Daemon/MobileNodeKeepaliveDaemon.java
+++ Project/src/main/java/Business/MobileNetworkNode/Daemon/MobileNodeKeepaliveDaemon.java
@@ -2,24 +2,31 @@ package Business.MobileNetworkNode.Daemon;
 
 import Business.MobileNetworkNode.PeerKeepaliveTable;
 import Business.MobileNetworkNode.MobileNode;
+import Business.MobileNetworkNode.RoutingInfo.RoutingTable;
 import org.apache.log4j.Logger;
 
 import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.List;
 
+/**
+ * Daemon responsible for, in behalf of a mobile node, testing for the presence
+ * of his peers and, if they are unable to prove so, remove them
+ */
 public class MobileNodeKeepaliveDaemon implements MobileNodeDaemon {
     private boolean finished;
-    private MobileNode representativeNode;
-    private PeerKeepaliveTable keepaliveTable;
+    private final MobileNode representativeNode;
+    private final PeerKeepaliveTable keepaliveTable;
+    private final RoutingTable contentRoutingTable;
     private final int KEEPAWAY_TIME_MS = 5000;
     private final int HELLO_FLOODING_PERIODICITY = 3; // every 3 KEEPARAY_TIME_MS, flood with HELLO
-    private Logger LOGGER;
+    private final Logger LOGGER;
 
     public MobileNodeKeepaliveDaemon(MobileNode representativeNode) {
         this.finished = false;
         this.representativeNode = representativeNode;
         this.keepaliveTable = representativeNode.getPeerKeepaliveTable();
+        this.contentRoutingTable = representativeNode.getRoutingTable();
         this.LOGGER = representativeNode.getLogger();
     }
 
@@ -34,7 +41,7 @@ public class MobileNodeKeepaliveDaemon implements MobileNodeDaemon {
         finished = true;
     }
 
-    void queryPeers() {
+    private void queryPeers() {
 
         int period = 0;
 
@@ -65,9 +72,20 @@ public class MobileNodeKeepaliveDaemon implements MobileNodeDaemon {
 
     private void removeDeadPeers() {
         List<String> removedPeers;
+
+        // Temove idle peers from the keepalive table
         synchronized (keepaliveTable) {
             removedPeers = keepaliveTable.applyStrikeWave();
         }
+
+        // The previous method returns all the peers that were removed
+        // All that is left is to remove all references of those peers
+        // From the content table
+        synchronized (contentRoutingTable) {
+            removedPeers.forEach(peer -> contentRoutingTable.removePeer(peer));
+        }
+
+
         LOGGER.info("Removed peers: " + removedPeers.toString());
     }
 
