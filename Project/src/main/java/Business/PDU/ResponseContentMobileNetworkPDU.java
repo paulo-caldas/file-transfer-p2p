5e864c82eaf54ef16e0b0f4076182efc231107e5
MDingas <a79089@alunos.uminho.pt>
2019-04-29 15:49:23 +0100

    :construction: data field is a String for testing, for now

--- Project/src/main/java/Business/PDU/ResponseContentMobileNetworkPDU.java
+++ Project/src/main/java/Business/PDU/ResponseContentMobileNetworkPDU.java
@@ -3,8 +3,13 @@ package Business.PDU;
 public class ResponseContentMobileNetworkPDU extends ContentMobileNetworkPDU {
 
     // Data data;
+    String data;
 
-    public ResponseContentMobileNetworkPDU(String srcMAC, String dstMAC, MobileNetworkMessageType messageType, MobileNetworkErrorType errorCode, int TTL, String sessionID, String contentID) {
-        super(srcMAC, dstMAC, messageType, errorCode, TTL, sessionID, contentID);
+    public ResponseContentMobileNetworkPDU(String srcMAC, String dstMAC, MobileNetworkMessageType messageType, MobileNetworkErrorType errorCode, int TTL, String sessionID, String[] routerPath, String data) {
+        super(srcMAC, dstMAC, messageType, errorCode, TTL, sessionID, routerPath);
+        this.data = data;
     }
+
+    public String getData() { return this.data; }
+    public void setData(String data) { this.data = data; }
 }
