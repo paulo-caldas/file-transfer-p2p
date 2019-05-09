58ed5cfe07258c46cd0c1cb9432b23271ad060db
MDingas <a79089@alunos.uminho.pt>
2019-05-09 23:30:37 +0100

    :art: corrected typo in method

--- Project/src/main/java/Main.java
+++ Project/src/main/java/Main.java
@@ -5,17 +5,20 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        // Parse arguments
         if (args.length != 1) {
             System.out.println("Usage: java -jar MobileTransfer.jar <directory of content to share>");
             System.exit(0);
         }
+
+        // First argument is the path where the files the user wants to share are
         File sharingDirectory = new File(args[0]).getAbsoluteFile();
 
         System.out.println("- Starting main");
 
         try {
             MobileNode node = new MobileNode(sharingDirectory);
-            node.autonamousStart();
+            node.autonomousStart();
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
